9c54cf094ad7abc0f7b295a77d279cc64d5e4cf7
Jonas Vadebo <jonas.vadebo@king.com>
2023-05-08 18:42:03 +0200

    Added response struct and json marshaller

--- main.go
+++ main.go
@@ -1,27 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+type Response struct {
+	Topic string `json:"topic"`
+	Time  int64  `json:"time"`
+}
 
 func main() {
 	router := mux.NewRouter()
 
 	fmt.Println(router)
 
+	//response := Response{"Jonas", time.Now().UnixMicro()}
+
+	var response Response
+
 	router.HandleFunc("/{topic}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		w.Write([]byte("Topic: " + vars["topic"]))
+
+		response = Response{vars["topic"], time.Now().UnixMilli()}
+		b, err := json.Marshal(response)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Write(b)
 	})
 	http.Handle("/", router)
 
-	err := http.ListenAndServe(":8080", nil)
-
-	if err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 
